0e5b3ed1a2a89507f90cfe674ecc2ff9c5b4713e
Davidmotato <davidmotato747@gmail.com>
2024-04-03 20:14:48 -0500

    Se agregan todos los archivos

---
+++ CalificacionesyAsistencia.js
@@ -0,0 +1,17 @@
+function verificarAprobacion(calificacion, asistencias) {
+    if (asistencias >= 80) {
+        if (calificacion >= 5) { 
+            return "Aprobado";
+        } else {
+            return "Reprobado";
+        }
+    } else {
+        return "Reprobado";
+    }
+}
+
+let calificacion = parseFloat(prompt("Ingrese la calificación del alumno: "));
+let asistencias = parseFloat(prompt("Ingrese el porcentaje de asistencias del alumno: "));
+
+let resultado = verificarAprobacion(calificacion, asistencias);
+alert("El alumno está " + resultado);
